a0a27d82ce9a64a6e86e6ce6fbbaf329a224153d
Andrea Trussardi <andrea@>
2022-05-23 16:56:38 +0200

    Sprint2

--- optica_database.sql
+++ optica_database.sql
@@ -3,13 +3,13 @@ CREATE DATABASE opticaDatabase;
 USE opticaDatabase;
 
 CREATE TABLE suppliers (
-    supplierId INT NOT NULL AUTO_INCREMENT,
-    supplierName VARCHAR(30) NOT NULL,
-    addressId INT NOT NULL,
-    supplierTelephone VARCHAR(16) NOT NULL,
-    supplierFax VARCHAR(16) NULL,
-    supplierNIF VARCHAR(16) NOT NULL,
-    PRIMARY KEY(supplierId)
+supplierId INT NOT NULL AUTO_INCREMENT,
+supplierName VARCHAR(30) NOT NULL,
+addressId INT NOT NULL,
+supplierTelephone VARCHAR(16) NOT NULL,
+supplierFax VARCHAR(16) NULL,
+supplierNIF VARCHAR(16) NOT NULL,
+PRIMARY KEY(supplierId)
 );
 INSERT INTO suppliers
 VALUES(supplierId ,'PincoPalla', 1, '+34 926 123 456', '+34 678 567 567', '567456N');
