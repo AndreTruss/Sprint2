ba9b6152a63582ade39d2d60776972a35fb166f3
Andrea Trussardi <andrea@>
2022-06-02 19:21:21 +0200

    query tienda y universidad

--- schema_universidad.sql
+++ schema_universidad.sql
@@ -257,3 +257,48 @@ INSERT INTO alumno_se_matricula_asignatura VALUES (19, 7, 5);
 INSERT INTO alumno_se_matricula_asignatura VALUES (19, 8, 5);
 INSERT INTO alumno_se_matricula_asignatura VALUES (19, 9, 5);
 INSERT INTO alumno_se_matricula_asignatura VALUES (19, 10, 5);
+
+
+-- QUERY
+
+-- 1.  Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
+SELECT apellido1, apellido2, nombre FROM persona WHERE tipo LIKE 'alumno' ORDER BY apellido1 ASC;
+
+-- 2. Esbrina el nom i els dos cognoms dels alumnes que no han donat d'alta el seu número de telèfon en la base de dades.
+SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo LIKE 'alumno' AND telefono IS NULL;
+
+-- 3. Retorna el llistat dels alumnes que van néixer en 1999.
+SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo LIKE 'alumno' AND fecha_nacimiento LIKE '1999%';
+
+-- 4. Retorna el llistat de professors que no han donat d'alta el seu número de telèfon en la base de dades i a més la seva nif acaba en K.
+SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo LIKE 'profesor' AND nif LIKE '%k' AND telefono IS NULL;
+
+-- 5. Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7.
+SELECT nombre FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
+
+-- 6. Retorna un llistat dels professors juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom.
+SELECT persona.apellido1, persona.apellido2, persona.nombre, departamento.nombre FROM persona INNER JOIN profesor ON persona.id = profesor.id_profesor INNER JOIN departamento ON profesor.id_departamento = departamento.id ORDER BY apellido1 ASC;
+
+-- 7. Retorna un llistat amb el nom de les assignatures, any d'inici i any de fi del curs escolar de l'alumne amb nif 26902806M.
+SELECT persona.nif, asignatura.nombre, curso_escolar.anyo_inicio, curso_escolar.anyo_fin FROM persona INNER JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_alumno = persona.id
+INNER JOIN asignatura ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id
+INNER JOIN curso_escolar ON alumno_se_matricula_asignatura.id_curso_escolar = curso_escolar.id
+WHERE persona.nif LIKE '26902806M';
+
+-- 8. Retorna un llistat amb el nom de tots els departaments que tenen professors que imparteixen alguna assignatura en el Grau en Enginyeria Informàtica (Pla 2015).
+SELECT DISTINCT departamento.nombre AS 'Nombre Departamento' FROM departamento
+INNER JOIN profesor ON departamento.id = profesor.id_departamento
+INNER JOIN asignatura ON asignatura.id_profesor = profesor.id_profesor
+INNER JOIN grado ON asignatura.id_grado = grado.id
+WHERE grado.nombre LIKE '%informatica%';
+
+-- 9. Retorna un llistat amb tots els alumnes que s'han matriculat en alguna assignatura durant el curs escolar 2018/2019.
+SELECT DISTINCT persona.nombre, persona.apellido1, persona.apellido2 FROM persona 
+INNER JOIN alumno_se_matricula_asignatura ON alumno_se_matricula_asignatura.id_alumno = persona.id
+INNER JOIN asignatura ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id
+INNER JOIN curso_escolar ON alumno_se_matricula_asignatura.id_curso_escolar = curso_escolar.id
+WHERE curso_escolar.anyo_inicio = 2018;
+
+
+-- 1. Retorna un llistat amb els noms de tots els professors i els departaments que tenen vinculats. El llistat també ha de mostrar aquells professors que no tenen cap departament associat. El llistat ha de retornar quatre columnes, nom del departament, primer cognom, segon cognom i nom del professor. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, cognoms i el nom.
+SELECT departamento.nombre, persona.apellido1, persona.apellido2, persona.nombre FROM departamento LEFT JOIN profesor ON profesor.id_departamento = departamento.id  LEFT JOIN persona ON persona.id = profesor.id_profesor ORDER BY departamento.nombre ASC;
